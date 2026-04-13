4e9b3838a3273190e99ea31876e303c8a8572fd6
HoangVanQuocNhat <hoangnhat250606@gmail.com>
2026-04-13 17:11:40 +0700

    Delete HoangVanQuocNhat_BTTrenLop directory

--- HoangVanQuocNhat_BTTrenLop/HoangVanQuocNhat_BTTrenLop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HoangVanQuocNhat_BTTrenLop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
